77deda9ad200e9ebc948ed7d19d88f7a011a918e
injekt <injekt.to@hotmail.com>
2015-07-02 03:08:25 +0000

    update

--- env.php
+++ env.php
@@ -2,5 +2,5 @@
 
 define("OAPI_HOST", "https://oapi.dingtalk.com");
 
-define("CORPID", "dingcc2b8da8d41f176f");
-define("SECRET", "jHViI9R-JlETwZr5OVmQEuhQ7WggXZAGSaacE9_XIHlsUJeGoeYb2VBB3jE9fYlk");
+define("CORPID", "");
+define("SECRET", "");
