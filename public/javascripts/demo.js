95da2a06e8942b972c53ff63645ec072aa2d75dc
injekt <injekt.to@hotmail.com>
2015-08-21 03:34:22 +0000

    update

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -21,61 +21,41 @@ dd.config({
         'biz.ding.post']
 });
 
-document.addEventListener('runtimeready', function() {
-    logger.i('runtime ready');
-    try {
-        window.nuva.require('runtime.permission').requestAuthCode({
-            corpId: _config.corpId,
-            onSuccess: function (info) {
-                logger.i('authcode: ' + info.code);
-                $.ajax({
-                    url: '/userinfo.php?code=' + info.code,
-                    type: 'GET',
-                    success: function (data, status, xhr) {
-                        var info = JSON.parse(JSON.parse(data));
-                        if (info.errcode === 0) {
-                            logger.i('user id: ' + info.userid);
-                        }
-                        else {
-                            logger.e('auth error: ' + data);
-                        }
-                    },
-                    error: function (xhr, errorType, error) {
-                        logger.e(errorType + ', ' + error);
-                    }
-                });
-            },
-            onFail: function (err) {
-                logger.e('fail: ' + JSON.stringify(err));
-            }
-        });
-    }
-    catch(e) {
-        logger.e(e);
-    }
-});
 
 dd.ready(function() {
     logger.i('dd.ready rocks!');
-
-    document.addEventListener('pause', function() {
-        logger.i('pause');
-    });
-
-    document.addEventListener('resume', function() {
-        logger.i('resume');
-    });
-
-    document.addEventListener('backbutton', function(ev) {
-        ev.preventDefault();
-        logger.i('backbutton');
-    });
 
     dd.runtime.info({
         onSuccess: function(info) {
             logger.i('runtime info: ' + JSON.stringify(info));
         },
         onFail: function(err) {
+            logger.e('fail: ' + JSON.stringify(err));
+        }
+    });
+    
+    dd.runtime.permission.requestAuthCode({
+        corpId: _config.corpId,
+        onSuccess: function (info) {
+            logger.i('authcode: ' + info.code);
+            $.ajax({
+                url: '/userinfo.php?code=' + info.code,
+                type: 'GET',
+                success: function (data, status, xhr) {
+                    var info = JSON.parse(JSON.parse(data));
+                    if (info.errcode === 0) {
+                        logger.i('user id: ' + info.userid);
+                    }
+                    else {
+                        logger.e('auth error: ' + data);
+                    }
+                },
+                error: function (xhr, errorType, error) {
+                    logger.e(errorType + ', ' + error);
+                }
+            });
+        },
+        onFail: function (err) {
             logger.e('fail: ' + JSON.stringify(err));
         }
     });
