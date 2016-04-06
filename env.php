fb6dda8ce80ebcbf6945af86253150cfd30f1747
三衡 <kevin.ln@alibaba-inc.com>
2016-04-06 21:05:00 +0800

    修改文件名

--- env.php
+++ env.php
@@ -7,7 +7,7 @@ define("SECRET", "3zFyluBktC54268A8HneZYKV3hpnlVO2xi-L3J6WXcKIPeQhlqXkuQWw0ure78
 
 //Suite
 define("CREATE_SUITE_KEY", "suite4xxxxxxxxxxxxxxx");
-define("SUITE_KEY", "");
-define("SUITE_SECRET", "");
+define("SUITE_KEY", "suitevdxzn0ulyn4w5fuq");
+define("SUITE_SECRET", "jfjn5ym0cOo-RpPcQKDlKFbllaSxXtbcAaROT0NigWWWtU1CQmGfHrr5VLKs4Mh1");
 define("TOKEN", "q1w2e3");
 define("ENCODING_AES_KEY", "chtv3r467zv0iffm5vmk6vrurj4kv1xi3d4sdy04s0e");
