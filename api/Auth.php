912da42544740355e3c6e7919a524c20691ff191
三衡 <kevin.ln@alibaba-inc.com>
2016-04-06 11:52:19 +0800

    修改jsticket

--- api/Auth.php
+++ api/Auth.php
@@ -26,15 +26,15 @@ class Auth
       */
     public static function getTicket($accessToken)
     {
-        $jsticket = $_SESSION['js_ticket'];
+        /*$jsticket = $_SESSION['js_ticket'];
         if ($jsticket === "")
-        {
+        {*/
             $response = Http::get('/get_jsapi_ticket', array('type' => 'jsapi', 'access_token' => $accessToken));
             self::check($response);
             $ticket = $response->ticket;
             $_SESSION['js_ticket'] = $ticket;
-        }
-        return $jsticket;
+        //}
+        return $ticket;
     }
     
     
