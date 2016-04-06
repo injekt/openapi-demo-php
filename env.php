1de4e7d33664e4e4d5d0666bbdd4855bbbecbe73
三衡 <kevin.ln@alibaba-inc.com>
2016-04-06 11:45:57 +0800

    添加corpid

--- env.php
+++ env.php
@@ -5,7 +5,6 @@ define("OAPI_HOST", "https://oapi.dingtalk.com");
 define("CORPID", "dingcc89897952f44d93");
 define("SECRET", "3zFyluBktC54268A8HneZYKV3hpnlVO2xi-L3J6WXcKIPeQhlqXkuQWw0ure78kX");
 
-
 //Suite
 define("CREATE_SUITE_KEY", "suite4xxxxxxxxxxxxxxx");
 define("SUITE_KEY", "");
